0b85ebf0672c30d05abc92c90ee44dc92f3e0a72
Sergii Brytiuk <sergii.brytiuk@chisw.us>
2014-07-25 14:35:43 +0300

    migration to integer changed

--- db/migrate/20140623153742_change_weight_and_hair_color_to_integer_in_people.rb
+++ db/migrate/20140623153742_change_weight_and_hair_color_to_integer_in_people.rb
@@ -1,6 +1,6 @@
 class ChangeWeightAndHairColorToIntegerInPeople < ActiveRecord::Migration
   def change
-    change_column :people, :weight, :integer
+    change_column :people, :weight, 'integer USING CAST(weight AS integer)'
     change_column :people, :hair_color, :integer
   end
 end
