176f8d2321ae6ca50cba0ec3b4eb579a9604b9b9
Sergii Brytiuk <sergii.brytiuk@chisw.us>
2014-10-24 17:16:41 +0300

    changed migration for mysql syntax

--- db/migrate/20140623153742_change_weight_and_hair_color_to_integer_in_people.rb
+++ db/migrate/20140623153742_change_weight_and_hair_color_to_integer_in_people.rb
@@ -1,7 +1,7 @@
 class ChangeWeightAndHairColorToIntegerInPeople < ActiveRecord::Migration
   def change
-    integer = Rails.env == 'production' ? 'integer USING CAST(weight AS integer)' : :integer
-    change_column :people, :weight, integer
-    change_column :people, :hair_color, integer
+    # integer = Rails.env == 'production' ? 'integer USING CAST(weight AS integer)' : :integer
+    change_column :people, :weight, :integer
+    change_column :people, :hair_color, :integer
   end
 end
