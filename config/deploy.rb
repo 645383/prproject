e99e681a45ccb715421174e9663cd8c959bae85b
Sergii Brytiuk <sergii.brytiuk@chisw.us>
2014-10-27 13:51:48 +0200

    aws_old branch added

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, '~/deploy/apps/prproject'
 
 # Default value for :scm is :git
 set :scm, :git
-set :branch, 'master'
+set :branch, 'aws_old'
 # Default value for :format is :pretty
 set :format, :pretty
 
